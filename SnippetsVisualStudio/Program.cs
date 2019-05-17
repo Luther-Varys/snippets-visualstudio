1028d63492bfdb919adc23eb72c7916fa0b6d847
carlo bramini <braminicarlo@live.com>
2019-05-17 02:13:24 +0200

    first commit

---
+++ SnippetsVisualStudio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SnippetsVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
